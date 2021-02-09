19a597b7dbea216840b7a705d6e0a0590aca26a6
Prakash Manglanathan <pmnathan@gmail.com>
2021-02-09 15:23:20 -0500

    Update app.tf

--- app.tf
+++ app.tf
@@ -28,7 +28,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   name                = "example-machine"
   resource_group_name = azurerm_resource_group.app_dev_rg_test_01.name
   location            = azurerm_resource_group.app_dev_rg_test_01.location
-  count               = 1
+  count               = 5
   size                = "Standard_D1_v2"
   admin_username      = "adminuser"
   network_interface_ids = [
